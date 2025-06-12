beb2056889a770aeb8d1680c97a97727ef4951d0
bernaduarte <mabelu2002@gmail.com>
2025-06-12 16:11:08 -0300

    adicionando modo de resetar o banco de dados

--- app/database.py
+++ app/database.py
@@ -1,3 +1,8 @@
+import os
+
+if os.environ.get("RESET_DB") == "true" and os.path.exists("bank.db"):
+    os.remove("bank.db")
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
